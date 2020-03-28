8c9dee59cc29b6111c55dd42e7440cc33100560d
bonguedits <dhitang92@gmail.com>
2020-03-28 20:45:25 +0530

    Sitting Dance: Size Adjustment

--- assets/data/data.js
+++ assets/data/data.js
@@ -7,21 +7,24 @@ var data = {
         sampleFile: "https://www.youtube.com/embed/TK60Rik-P0M",
         downloadLink: "https://docs.google.com/uc?export=download&id=17RcD5uU9xFOBjVzmsw6iMEUQzJq4dFPd",
         downloadType: "video",
-        title: "Sci Fi Holo clock Yellow"
+        title: "Sci Fi Holo clock Yellow",
+        style: ""
       },
       {
         type: "image",
         sampleFile: "sitting-dance.gif",
         downloadLink: "https://docs.google.com/uc?export=download&id=1xkEo3t4DjkGCbuPL5rNhtu0DcVI-xPY3",
         downloadType: "photoshop",
-        title: "Dancing Boy"
+        title: "Dancing Boy",
+        style: "width: 160px;"
       },
       {
         type: "image",
         sampleFile: "ps-icon.png",
         downloadLink: "",
         downloadType: "photoshop",
-        title: "PS 1"
+        title: "PS 1",
+        style: ""
       }
     ]
   }
